243585d5339359a5716b50ce4e79943561ddf44b
Kyungrae <jeremiah.kim@hotmail.com>
2020-01-10 09:22:13 -0800

    Redux action completed

---
+++ ReactCSharpPractice/ReactCSharpPractice/ClientApp/src/actions/tripAction.js
@@ -0,0 +1,26 @@
+﻿import axios from 'axios';
+
+export const GET_ALL_TRIPS_REQUEST = 'GET_ALL_TRIPS_REQUEST';
+export const GET_ALL_TRIPS_SUCCESS = 'GET_ALL_TRIPS_SUCCESS';
+export const GET_ALL_TRIPS_ERROR = 'GET_ALL_TRIPS_ERROR';
+
+const getTripsSuccess = payload => ({
+    type: GET_ALL_GET_ALL_TRIPS_SUCCESS,
+    payload
+});
+
+const getTRipsError = payload => ({
+    type: GET_ALL_TRIPS_ERROR,
+    payload
+});
+
+export const getAllTrips = () => dispatch => {
+    dispatch({ type: GET_ALL_TRIPS_REQUEST });
+    return axios.get('api.Trips/GetTrips').then(res => {
+        const reponse = res.data;
+        dispatch(getTripsSuccess(response));
+    }).catch(error => {
+        dispatch(getTripsError("Something went wrong!"));
+        return Promise.reject({});
+    })
+}
