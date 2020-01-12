53de16fac1d698f2a93f54afc4525ec809a46441
Kyungrae <jeremiah.kim@hotmail.com>
2020-01-11 20:08:14 -0800

    New solution created due to React and Redux conflict

--- ReactCSharpPractice/ReactCSharpPractice/ClientApp/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-﻿import { combinedReducers } from 'redux';
-import tripReducers from './tripReducers';
-
-// root reducer file to export all the reducers so they are accessible from other parts of the app
-const rootReducer = combinedReducers({
-    trips: tripReducers
-});
-
-export default rootReducer;
--- ReactCSharpPractice/ReactCSharpPractice/Startup.cs
+++ ReactCSharpPractice/ReactCSharpPractice/Startup.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ReactCSharpPractice.Data.Services;
 
 namespace ReactCSharpPractice
 {
@@ -22,6 +20,7 @@ namespace ReactCSharpPractice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
@@ -29,8 +28,6 @@ namespace ReactCSharpPractice
             {
                 configuration.RootPath = "ClientApp/build";
             });
-
-            services.AddTransient<ITripService, TripService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
